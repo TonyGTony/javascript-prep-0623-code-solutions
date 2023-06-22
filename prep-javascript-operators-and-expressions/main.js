cc126de3e98dfcea962ab75bbf47c6f9d6727c1b
Antonio Gomez <zem.og7799@gmail.com>
2023-06-22 23:24:24 +0000

    Completed prep javscript operator and expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+const number1 = 5;
+const number2 = 2;
+const product = number1 * number2;
+console.log('value of product:', product);
+console.log('typeof product:', typeof product);
+
+const charge = 10;
+const payment = 2;
+const amountRemaining = payment - charge;
+console.log('value of amountRemaining:', amountRemaining);
+console.log('typeof amountRemaining:', typeof amountRemaining);
+
+const test = 45;
+const assignments = 23;
+const final = 100;
+
+const grade = (test + assignments + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Antonio';
+const lastName = 'Gomez';
+const fullName = `${firstName} ${lastName}`;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 8;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const numberOfSoldiers = 250;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('value of nickname:', nickname);
+console.log('typeof nickname:', typeof nickname);
